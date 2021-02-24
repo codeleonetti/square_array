04a3f54754079b25f64c3ca503091a1b36a041d3
john <john.d.leonetti@gmail.com>
2021-02-24 15:20:46 -0800

    commit tree 15484e7e8a6de45387dc4a7d26e382014291bebf

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,14 @@
 def square_array(array)
   # your code here
+  sqd_num = []
+  array.each do |numbers|
+    sqd_num.push(numbers ** 2)
+    
+  end
+  return sqd_num
 end
+# def square_array(array)
+#   sqd_num = []
+#   array.each { |numbers| sqd_num.push(numbers * numbers) }
+
+# end
